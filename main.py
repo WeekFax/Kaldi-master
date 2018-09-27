3716282d4ce716b4a51c45574edd2b3c2bcec393
WeekFax <we9697@mail.ru>
2018-09-27 23:08:49 +0600

    save commit

--- main.py
+++ main.py
@@ -18,17 +18,6 @@ def wav_to_vad(wav_file, vad_file, sr=8000):
     write_wav(vad_file, v.get_speech(audio), rate)
 
 
-def vad_all_files(to_dir_path, from_dir_path):
-    if not os.path.exists(to_dir_path):
-        os.mkdir(to_dir_path)
-
-    for file in os.listdir(from_dir_path):
-        if file.endswith(".wav"):
-            print("VADing file:",file)
-            wav_to_vad(os.path.join(from_dir_path,file),os.path.join(to_dir_path,file))
-            print("Done")
-
-
 def get_mfcc_from_file(file_name, mfcc_dir=None, config_name="vbdiar.yml"):
     if mfcc_dir is not None:
         if not os.path.isdir(mfcc_dir):
@@ -55,17 +44,6 @@ def get_mfcc_from_file(file_name, mfcc_dir=None, config_name="vbdiar.yml"):
     return features
 
 
-def mfcc_all_files(to_dir_path, from_dir_path):
-    if not os.path.exists(to_dir_path):
-        os.mkdir(to_dir_path)
-
-    for file in os.listdir(from_dir_path):
-        if file.endswith(".wav"):
-            print("Get MFCC from file:", file)
-            get_mfcc_from_file(os.path.join(from_dir_path,file),mfcc_dir=to_dir_path)
-            print("Done")
-
-
 def get_embeddings_from_features(features, config_name="vbdiar.yml"):
     config = Utils.read_config(config_name)
     config_embedding_extractor = config['EmbeddingExtractor']
@@ -84,40 +62,123 @@ def get_embeddings_from_features(features, config_name="vbdiar.yml"):
     return embeddings['1']
 
 
-def get_embeddings_from_file(features_file):
-    hf = h5py.File(features_file, 'r')
-    features=np.array(hf.get("mfcc"))
-    return get_embeddings_from_features(features)
+def get_mfcc(file_path):
+    audio, sr = librosa.load(file_path, sr = 16000)
+    mfcc=[]
+    while audio.shape[0]>=(sr*3):
+        audio_frame=audio[:(sr*3)]
+        mfcc.append(librosa.feature.mfcc(y=audio_frame, sr = sr, n_mfcc=23).T)
+        audio=audio[int(sr*3/2):]
+    audio_frame=np.zeros(sr*3,dtype=np.float32)
+    audio_frame[:audio.shape[0]]=audio
+    mfcc.append(librosa.feature.mfcc(y=audio_frame, sr=sr, n_mfcc=23).T)
+    return mfcc
 
 
-def embedding_all_file(to_dir_path, from_dir_path):
-    if not os.path.exists(to_dir_path):
-        os.mkdir(to_dir_path)
-    for file in os.listdir(from_dir_path):
-        if file.endswith(".wav.h5"):
-            print("Get embedding from file:", file)
-            emb=get_embeddings_from_file(os.path.join(from_dir_path,file))
-            hf = h5py.File(os.path.join(to_dir_path, file.split('/')[-1]), 'w')
-            hf.create_dataset('embeddings', data=emb)
-            hf.close()
-            print("Done")
+def compute_eer(probabilities, target, n=10000):
+    start_k = probabilities.min()
+    stop_k = probabilities.max()
+    proba_space = np.linspace(start_k, stop_k, n)
 
-def plda():
-    hf = h5py.File("embs/fe_03_00001-a.wav.h5", 'r')
-    embs = np.array(hf.get("embeddings"))
+    human_score = probabilities[np.logical_not(target == 0)]
+    spoof_score = probabilities[target == 0]
 
-    classifier = Classifier()
-    classifier.fit_model()
-    U_model = classifier.model.transform(embs, from_space='D', to_space='U_model')
+    n_human = human_score.size
+    n_spoof = spoof_score.size
 
+    frr = np.empty(n)
+    far = np.empty(n)
+    n_eer = n // 2
+    eer = 1.0
 
-    pass
+    min_gap = np.inf
+    for m, proba in enumerate(proba_space):
+        frr[m] = len(np.where(human_score >= proba)[0]) / n_human
+        far[m] = len(np.where(spoof_score < proba)[0]) / n_spoof
+
+        gap = np.abs(far[m] - frr[m])
+
+        if gap < min_gap:
+            min_gap = gap
+            n_eer = m
+            eer = (far[m] + frr[m]) / 2
+
+    return (n_eer, eer), frr, far, proba_space
+
 
 if __name__ == '__main__':
-    #vad_all_files(from_dir_path="wav", to_dir_path="vad")
-    #mfcc_all_files(from_dir_path="vad", to_dir_path="MFCCs")
-    #embedding_all_file(from_dir_path="MFCCs", to_dir_path="embs")
 
-    plda()
+    """
+    files = [line.rstrip('\n') for line in open("ASVspoof2017_dev.trl.txt")]
+    for file in files:
+        file=file.split(' ')[0]
+        mfccs = get_mfcc(os.path.join("../ASVspoof2017_dev",file))
+        emb=[]
+        for mfcc in mfccs:
+            emb.append(get_embeddings_from_features(mfcc, config_name="vbdiar.yml"))
+        xv=np.array(emb)
+        xv=np.mean(xv,axis=0)
+        hf = h5py.File(os.path.join("embs/dev","{}.{}".format(file,"h5")))
+        hf.create_dataset('xv',data=xv)
+        hf.close()
+        print(file)
+    """
+    classifier=Classifier()
+
+    train_data=[]
+    train_label=[]
+
+    files = [line.rstrip('\n') for line in open("ASVspoof2017_train.trn.txt")]
+    for file in files:
+        file_sp=file.split(' ')
+        file_name=file_sp[0]
+        file_label=file_sp[1]
+        file_M=file_sp[2]
+        file_S=file_sp[3]
+
+        hf=h5py.File(os.path.join("embs/train","{}.{}".format(file_name,"h5")),'r')
+        xv=np.array(hf.get("xv"))
+        hf.close()
+
+        train_data.append(xv)
+        if(file_label=="spoof"):
+            train_label.append(0)
+        if (file_label == "genuine"):
+            train_label.append(1)
+
+    train_label=np.array(train_label)
+    train_data=np.array(train_data)
+
+
+    test_data=[]
+    test_label=[]
+    files = [line.rstrip('\n') for line in open("ASVspoof2017_dev.trl.txt")]
+    for file in files:
+        file_sp = file.split(' ')
+        file_name = file_sp[0]
+        file_label = file_sp[1]
+        file_M = file_sp[2]
+        file_S = file_sp[3]
+
+        hf = h5py.File(os.path.join("embs/dev", "{}.{}".format(file_name, "h5")), 'r')
+        xv = np.array(hf.get("xv"))
+        hf.close()
+
+        test_data.append(xv)
+        if (file_label == "spoof"):
+            test_label.append(0)
+        if (file_label == "genuine"):
+            test_label.append(1)
+
+    test_label = np.array(test_label)
+    test_data = np.array(test_data)
+
+    classifier.fit_model(train_data,train_label,n_principal_components=90)
+
+    predictions, log_p_predictions = classifier.predict(test_data)
+    (n_eer, eer), frr, far, proba_space=compute_eer(log_p_predictions,test_label)
+
+    print('Accuracy: {}'.format((test_label == predictions).mean()))
+    print('Eer {}'.format(eer))
 
     pass
